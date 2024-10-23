8c967ff618fd4a98912580e5df85528c8f8fdc13
rakibislam2233 <rakib2020.tkg@gmail.com>
2024-10-23 17:52:24 +0600

    terms-conditions page created

---
+++ src/app/(mainLayout)/terms-conditions/page.tsx
@@ -0,0 +1,8 @@
+import TermsConditions from '@/components/Pages/Others/TermsConditions/TermsConditions'
+import React from 'react'
+
+const page = () => {
+  return  <TermsConditions/>
+}
+
+export default page
